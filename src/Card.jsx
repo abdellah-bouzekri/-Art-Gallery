a712205e380570cf536cbcfefa1ea233cea7f530
abdellahbouzekri <abdo.bouzekri123@gmail.com>
2024-08-09 13:55:09 +0100

    adding react lazy load2

--- src/Card.jsx
+++ src/Card.jsx
@@ -27,37 +27,39 @@ export default function Card() {
             />
           ))}
         </div>
-
-        <h1 className="title">Galleries of Grandeur</h1>
-        <div
-          id="art-gallery"
-          className="AImges"
-          style={{ background: theme === "light" ? "#fff" : "black" }}>
-          {museumArtsImages.map((image, index) => (
-            <LazyLoadImage
-              className="img"
-              effect="blur"
-              src={image}
-              key={index}
-              alt={`Museum Art Image ${index + 1}`}
-            />
-          ))}
+        <div>
+          <h1 className="title">Galleries of Grandeur</h1>
+          <div
+            id="art-gallery"
+            className="AImges"
+            style={{ background: theme === "light" ? "#fff" : "black" }}>
+            {museumArtsImages.map((image, index) => (
+              <LazyLoadImage
+                className="img"
+                effect="blur"
+                src={image}
+                key={index}
+                alt={`Museum Art Image ${index + 1}`}
+              />
+            ))}
+          </div>
         </div>
-
-        <h1 className="title">The Art of Innovation</h1>
-        <div
-          id="art-gallery"
-          className="AImges"
-          style={{ background: theme === "light" ? "#fff" : "black" }}>
-          {technologyImages.map((image, index) => (
-            <LazyLoadImage
-              className="img"
-              effect="blur"
-              src={image}
-              key={index}
-              alt={`Technology Image ${index + 1}`}
-            />
-          ))}
+        <div>
+          <h1 className="title">The Art of Innovation</h1>
+          <div
+            id="art-gallery"
+            className="AImges"
+            style={{ background: theme === "light" ? "#fff" : "black" }}>
+            {technologyImages.map((image, index) => (
+              <LazyLoadImage
+                className="img"
+                effect="blur"
+                src={image}
+                key={index}
+                alt={`Technology Image ${index + 1}`}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </>
